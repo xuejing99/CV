71abd8624399af6f940f1d6b87466e35d527c11c
xuejing99 <48306154+xuejing99@users.noreply.github.com>
2021-08-27 10:28:26 +0800

    Update onnx2openvino.sh

--- Deployment/YOLOv5/onnx2openvino.sh
+++ Deployment/YOLOv5/onnx2openvino.sh
@@ -1,1 +1,2 @@
-mo --input_model weights/yolov5s.onnx --model_name weights/yolov5s --output Conv_251,Conv_559,Conv_867
+# mo --input_model weights/yolov5s.onnx --model_name weights/yolov5s --output Conv_251,Conv_559,Conv_867
+mo --input_model weights/yolov5s.onnx --model_name weights/yolov5s --output output
